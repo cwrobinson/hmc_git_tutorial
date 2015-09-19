918f131249b5020133ab7f3143d06560caa4d91c
Charlotte Robinson <cwrobinson@hmc.edu>
2015-09-18 22:18:45 -0700

    added test.m

---
+++ test.m
@@ -0,0 +1,8 @@
+;; This buffer is for notes you don't want to save, and for Lisp evaluation.
+;; If you want to create a file, visit that file with C-x C-f,
+;; then enter the text in that file's own buffer.
+
+x=[0,1,2,3,4,5]
+y=[0,2,4,6,8,10]
+fs = 50
+impulse_response[x,y,fs]
